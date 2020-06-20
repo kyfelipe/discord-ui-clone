9be6c75612744978d8a2cd4f05046f1710aeaf7d
Felipe Côrtes <contatos.cortes@gmail.com>
2020-06-20 08:34:16 -0300

    Create layout grid

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Grid } from './styles'
+
+// export default function Layout() { // JS
+//   return (
+//     <div>Oi mundo!</div>
+//   );
+// }
+
+const Layout: React.FC = () => {
+  return (
+    <Grid />
+  );
+};
+
+export default Layout;
